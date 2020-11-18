4d0fddd15ddfe7289f4a7e6541494e42561c7f08
Manfred Touron <94029+moul@users.noreply.github.com>
2020-11-18 13:46:56 +0100

    feat: add base58 and alternative base64
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- conv_encoding.go
+++ conv_encoding.go
@@ -11,12 +11,19 @@ import (
 	"strings"
 
 	"github.com/BurntSushi/toml"
+	"github.com/mr-tron/base58"
 )
 
 // nolint:gochecknoinits // need a refactor to remove it
 func init() {
+	RegisterConverter(NewConverter("base58-encode").SetTypes("[]byte", "string").SetConversionFunc(ConvertBytesToBase58))
+	RegisterConverter(NewConverter("base58-decode").SetTypes("string", "[]byte").SetConversionFunc(ConvertBase58ToBytes))
 	RegisterConverter(NewConverter("base64-encode").SetTypes("[]byte", "string").SetConversionFunc(ConvertBytesToBase64))
 	RegisterConverter(NewConverter("base64-decode").SetTypes("string", "[]byte").SetConversionFunc(ConvertBase64ToBytes))
+	RegisterConverter(NewConverter("urlbase64-encode").SetTypes("[]byte", "string").SetConversionFunc(ConvertBytesToURLBase64))
+	RegisterConverter(NewConverter("urlbase64-decode").SetTypes("string", "[]byte").SetConversionFunc(ConvertURLBase64ToBytes))
+	RegisterConverter(NewConverter("rawurlbase64-encode").SetTypes("[]byte", "string").SetConversionFunc(ConvertBytesToRawURLBase64))
+	RegisterConverter(NewConverter("rawurlbase64-decode").SetTypes("string", "[]byte").SetConversionFunc(ConvertRawURLBase64ToBytes))
 	RegisterConverter(NewConverter("base32-encode").SetTypes("[]byte", "string").SetConversionFunc(ConvertBytesToBase32))
 	RegisterConverter(NewConverter("base32-decode").SetTypes("string", "[]byte").SetConversionFunc(ConvertBase32ToBytes))
 	RegisterConverter(NewConverter("hex-encode").SetTypes("[]byte", "string").SetConversionFunc(ConvertBytesToHex))
@@ -29,6 +36,26 @@ func init() {
 	RegisterConverter(NewConverter("csv-decode").SetTypes("string", "[][]string").SetConversionFunc(ConvertStringToCsv))
 }
 
+func ConvertBytesToRawURLBase64(in interface{}, out *interface{}) error {
+	*out = base64.RawURLEncoding.EncodeToString(in.([]byte))
+	return nil
+}
+
+func ConvertRawURLBase64ToBytes(in interface{}, out *interface{}) (err error) {
+	*out, err = base64.RawURLEncoding.DecodeString(in.(string))
+	return err
+}
+
+func ConvertBytesToURLBase64(in interface{}, out *interface{}) error {
+	*out = base64.URLEncoding.EncodeToString(in.([]byte))
+	return nil
+}
+
+func ConvertURLBase64ToBytes(in interface{}, out *interface{}) (err error) {
+	*out, err = base64.URLEncoding.DecodeString(in.(string))
+	return err
+}
+
 func ConvertBytesToBase64(in interface{}, out *interface{}) error {
 	*out = base64.StdEncoding.EncodeToString(in.([]byte))
 	return nil
@@ -36,6 +63,16 @@ func ConvertBytesToBase64(in interface{}, out *interface{}) error {
 
 func ConvertBase64ToBytes(in interface{}, out *interface{}) (err error) {
 	*out, err = base64.StdEncoding.DecodeString(in.(string))
+	return err
+}
+
+func ConvertBytesToBase58(in interface{}, out *interface{}) error {
+	*out = base58.Encode(in.([]byte))
+	return nil
+}
+
+func ConvertBase58ToBytes(in interface{}, out *interface{}) (err error) {
+	*out, err = base58.Decode(in.(string))
 	return err
 }
 
